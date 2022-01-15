17a4e33f1f54bc62cd4871e419cc57fd4c8cf6c6
skdksldk <tnwns1485@naver.com>
2022-01-15 10:54:58 +0900

    add register page function

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import clayful from 'clayful/client-js';
 
 function RegisterPage() {
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
+
+
+      const handleEmailChange = (event) => {
+          setEmail(event.target.value);
+      };
+
+      const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        var Customer = clayful.Customer;
+
+        var payload = {
+        email,
+        password,
+    };
+    console.log('payload',payload);
+
+    Customer.createMe(payload, function(err, result) {
+        if (err) {
+        // Error case
+        console.log(err.code);
+        }
+
+        var data = result.data;
+
+        console.log(data);
+
+        });
+    };
+
+
     return (
         <div className="auth-wrapper">
         <h1>회원가입.</h1>
-        <form>
-            <input placeholder="Apple Id" type="email" name="email" value=""/>
-            <input placeholder="암호" type="password" name="password" value=""/>
+        <form onSubmit={handleSubmit}>
+            <input 
+            onChange={handleEmailChange}
+            placeholder="Apple Id"
+            type="email" 
+            name="email" 
+            value={email}
+            />
+            <input 
+            onChange={handlePasswordChange}
+            placeholder="암호" 
+            type="password" 
+            name="password" 
+            value={password}
+            />
 
             <button type="submit">회원가입.</button>
             <Link to="login" style={{ color:"gray", textDecoration: "none"}}>
