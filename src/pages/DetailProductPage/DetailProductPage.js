c840611a5339c2ee4c013e27427f75b11566e9dc
skdksldk <tnwns1485@naver.com>
2022-01-15 17:41:39 +0900

    show upload img

--- src/pages/DetailProductPage/DetailProductPage.js
+++ src/pages/DetailProductPage/DetailProductPage.js
@@ -1,6 +1,8 @@
 import React, { useState , useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import clayful from "clayful/client-js";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function DetailProductPage() {
     const params = useParams();
@@ -22,7 +24,16 @@ function DetailProductPage() {
     }, []);
 
     return (
-    <div>
+    <div className="pageWrapper">
+        <Row>
+            <Col md>
+                <div>
+                    <img src={item.thumbnail?.url} alt={item.name} />
+                </div>
+            </Col>
+            <Col md></Col>
+        </Row>
+
         <div dangerouslySetInnerHTML={{__html: item.description}} />
     </div>
     );
