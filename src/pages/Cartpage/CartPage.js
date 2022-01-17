0d8f59d84e34b3d632a7cec199e02cbe413bf070
skdksldk <tnwns1485@naver.com>
2022-01-17 09:52:20 +0900

    add cart 1

--- src/pages/Cartpage/CartPage.js
+++ src/pages/Cartpage/CartPage.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import clayful from "clayful/client-js";
+import {useNavigate} from 'react-router-dom';
 
 function CartPage() {
+    const navigate = useNavigate();
+    const [cart, setCart] = useState({});
+
+    useEffect(() => {
+        var Cart = clayful.Cart;
+
+        var options = {
+            customer: localStorage.getItem("accessToken"),
+        };
+        
+        Cart.getForMe({}, options, function(err, result) {
+        
+            if (err) {
+                // Error case
+                console.log(err.code);
+                return;
+            }
+            var data = result.data;
+            setCart(data.cart) 
+      });
+    }, []);
+
+    const items = cart.items;
     return (
-        <div>
-            CartPage
+      <div className="pageWrapper">
+        <div className="shopping-cart">
+            <h1 className="title">장바구니</h1>
+
+            <div className="shopping-cart-body" style={{ minHeight:100}}> </div>
+            
+            {items && items.length > 0 && (
+             <div className="bottom">
+                <span className="total-price">
+                    총 금액: ₩ {cart.total?.amount.raw}
+                </span>
+                <button 
+                   style={{ float: 'right', padding: '4px 8px'}}
+                   onClick={()=> navigate("/payment")}
+                >
+                    결재
+                </button>   
+             </div>
+        )}
         </div>
-    )
+    </div>
+    );
 }
+
 
 export default CartPage;
