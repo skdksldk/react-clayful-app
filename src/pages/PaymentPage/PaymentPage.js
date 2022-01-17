b498003fd223e650b437e07eaa0511b805500697
skdksldk <tnwns1485@naver.com>
2022-01-17 19:44:33 +0900

    design payment page

--- src/pages/PaymentPage/PaymentPage.js
+++ src/pages/PaymentPage/PaymentPage.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import clayful from 'clayful/client-js';
+import "./PaymentPage.css";
 
 function PaymentPage() {
 
@@ -24,7 +25,7 @@ function PaymentPage() {
 	var data = result.data;
     setPaymentMethods(data);
     });
-}
+};
     const getCartData =() => {
         var Cart = clayful.Cart;
 
@@ -39,14 +40,56 @@ function PaymentPage() {
             }
 	        var data = result.data;
             setCart(data.cart);
-	        console.log(data);
         });
     };
+    
     return (
-        <div>
-            PaymentPage
+    <div className="pageWrapper">
+        <div className="payment">
+      <div 
+          style={{
+               width: "100%", 
+               display: "flex", 
+               borderBottom: "1px solid #d2d2d7",
+            }}
+        >
+            <div style={{ width:'50%', fontSize: 24, fontWeight: 500 }}> 결재 </div>
+            <div style={{ width:'50%', display: 'flex', justifyContent: 'end' }}> 
+            주문 총 가격: {cart.total?.amount.raw + 3000}원 (3000원 배송비)
+            </div>
         </div>
-    )
-}
 
+         <div style={{ marginTop:16, width:'100', display: 'flex'}}>
+            <div style={{ width:'49%' }}> 
+            <h5>주문자 정보</h5>
+            <input type="text" name="full" placeholder="주문자명"/>
+            <input type="text" name="mobile" placeholder="무선 연락처"/>
+            <div>
+                <input type="checkbox" id="sameInfo" name="sameInfo" />
+                <label htmlFor="sameInfo">수취자 정보도 위와 동일합니다.</label> 
+            </div>
+            </div>
+            <div style={{ width:'2%' }} /> 
+            <div style={{ width:'49%' }}> 
+            <h5>수취자 정보</h5>
+            <input type="text" name="full" placeholder="수취자명"/>
+            <input type="text" name="mobile" placeholder="무선 연락처"/>
+
+            <h5>배송 정보</h5>
+            <input type="text" readOnly placeholder="주소"/>
+            <input type="text" name="address2" placeholder="상세 주소"/>
+            <input type="text" readOnly placeholder="우편번호"/>
+
+            <h5>결재</h5>
+            <select>
+                <option>옵션</option>
+            </select>
+           
+            <button style={{ width: "100%", marginTop: 10 }}>주문</button>
+          </div>
+        </div>
+      </div>
+    </div> 
+    );
+} 
 export default PaymentPage;
