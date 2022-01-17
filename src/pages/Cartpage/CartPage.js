3c83d66b2601248968fd087fb70c234268a12a3e
skdksldk <tnwns1485@naver.com>
2022-01-17 11:31:34 +0900

    add cart 2

--- src/pages/Cartpage/CartPage.js
+++ src/pages/Cartpage/CartPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import clayful from "clayful/client-js";
 import {useNavigate} from 'react-router-dom';
+import "./CartPage.css";
+import CartItem from "./Sections/CartItem";
 
 function CartPage() {
     const navigate = useNavigate();
@@ -26,14 +28,30 @@ function CartPage() {
     }, []);
 
     const items = cart.items;
+    console.log('items',items);
     return (
       <div className="pageWrapper">
         <div className="shopping-cart">
             <h1 className="title">장바구니</h1>
 
-            <div className="shopping-cart-body" style={{ minHeight:100}}> </div>
+            <div className="shopping-cart-body" style={{ minHeight:100}}> 
+            {items && items.length > 0 ? (
+                    items.map((item, index) => {
+                        return <CartItem 
+                            key= {item._id}
+                            item={item}
+                            index={index}
+                        />;
+                    })
+                ): (
+                  <p style={{ textAlign: "center", marginTop: "2rem"}}>
+                      카트에 상품이 하나도 없습니다.
+                  </p>
+                )}
+            </div>
             
-            {items && items.length > 0 && (
+            {items && items.length > 0  && (
+
              <div className="bottom">
                 <span className="total-price">
                     총 금액: ₩ {cart.total?.amount.raw}
